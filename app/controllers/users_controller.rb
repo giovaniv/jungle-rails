c5168b45eac2059660dc50ad9859b919e7078f35
giovaniv <giovaniv@gmail.com>
2018-07-21 16:14:49 +0000

    bugs fixed and check requirements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,28 @@ class UsersController < ApplicationController
   end
   
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      UserMailer.welcome_email(user:user).deliver_now
-      redirect_to '/'
+
+    email_exist = User.where(:email => user_params['email']).exists?
+    password = user_params['password']
+    password_check = user_params['password_confirmation']
+
+    if password != password_check
+      redirect_to '/signup', flash: { danger: 'Password and password confirmation must match' }
     else
-      redirect_to '/signup'
+      if email_exist
+        redirect_to '/signup', flash: { danger: 'Email already exists' }
+      else
+        user = User.new(user_params)
+        if user.save
+          session[:user_id] = user.id
+          UserMailer.welcome_email(user:user).deliver_now
+          redirect_to '/'
+        else
+          redirect_to '/signup'
+        end
+      end
     end
+
   end
 
   def user_params
